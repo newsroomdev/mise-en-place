709c1b28a6ff3162a77c14ef9d3b1cf7b37a77a7
geraldarthur <gerald.a.rich@gmail.com>
2014-05-23 21:50:24 -0400

    add the basics

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 git pull
+
+# homebrew, vim, zsh
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+curl https://j.mp/spf13-vim3 -L -o - | sh
+curl -L http://install.ohmyz.sh | sh
+
+# the rest
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
@@ -13,6 +21,7 @@ else
 		doIt
 	fi
 fi
+
 unset doIt
-curl https://j.mp/spf13-vim3 -L -o - | sh
+
 . ~/.zshrc
